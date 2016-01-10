d6b1679d08e3d5eab8400183de77c048754b00e1
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-01-10 17:52:26 +0100

    Rename QueryTemplate test to reflect SUT name

--- tests/src/Functional/QueryTemplateTest.php
+++ tests/src/Functional/QueryTemplateTest.php
@@ -24,7 +24,7 @@ use Symfony\Component\Finder\Finder;
  * @license http://opensource.org/licenses/MIT MIT
  * @package Hierarchy
  */
-class TemplateLoaderTest extends TestCase
+class QueryTemplateTest extends TestCase
 {
     public function testLoadPageCustom()
     {
@@ -74,5 +74,53 @@ class TemplateLoaderTest extends TestCase
         $loader->loadTemplate($wpQuery);
 
         assertSame('foo bar', trim(ob_get_clean()));
+    }
+
+    public function testFallbackToArchive()
+    {
+        Functions::when('get_stylesheet_directory')->alias(function () {
+            return getenv('HIERARCHY_TESTS_BASEPATH') . '/files/it_IT';
+        });
+
+
+        $wpQuery = new \WP_Query([
+            'is_tax'     => true,
+            'is_archive' => true,
+        ], (object) ['slug' => 'bar', 'taxonomy' => 'foo']);
+
+        $loader = new QueryTemplate();
+
+        ob_start();
+        $loader->loadTemplate($wpQuery);
+
+        assertSame('archive', trim(ob_get_clean()));
+    }
+
+    public function testFallbackToIndex()
+    {
+        Functions::when('get_stylesheet_directory')->alias(function () {
+            return getenv('HIERARCHY_TESTS_BASEPATH') . '/files';
+        });
+
+        $wpTaxQuery = new \WP_Query([
+            'is_tax'     => true,
+            'is_archive' => true,
+        ], (object) ['slug' => 'bar', 'taxonomy' => 'foo']);
+
+        $wpSearchQuery = new \WP_Query([
+            'is_search'     => true,
+        ]);
+
+        $loader = new QueryTemplate();
+
+        ob_start();
+        $loader->loadTemplate($wpTaxQuery);
+
+        assertSame('index', trim(ob_get_clean()));
+
+        ob_start();
+        $loader->loadTemplate($wpSearchQuery);
+
+        assertSame('index', trim(ob_get_clean()));
     }
 }
