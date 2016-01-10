fe09e33157e7de2b71ca3d5cd4e2be8a9d28369c
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-01-10 18:48:14 +0100

    Cosmetics and phpDoc

--- Hierarchy.php
+++ Hierarchy.php
@@ -3,25 +3,81 @@
 class Hierarchy
 {
     /**
+     * Array representation on template hierarchy.
+     * Format is:
+     *
+     * branch name => [
+     *      array of possible templates,
+     *      boolean indicating templates has variable names
+     * ]
+     *
      * @var array
      */
     private static $branches = [
-        '404'               => [['404'], false],
-        'search'            => [['search'], false],
-        'front_page'        => [['front-page'], false],
-        'home'              => [['home'], false],
-        'post_type_archive' => [['archive'], false],
-        'tax'               => [['taxonomy-{{tax}}-{{slug}}', 'taxonomy-{{tax}}', 'taxonomy'], true],
-        'attachment'        => [['attachment'], false],
-        'single'            => [['single-{{slug}}', 'single', 'singular'], true],
-        'page'              => [['page-{{slug}}', 'page-{{id}}', 'page', 'singular'], true],
-        'category'          => [['category-{{slug}}', 'category-{{id}}', 'category'], true],
-        'tag'               => [['tag-{{slug}}', 'tag-{{id}}', 'tag'], true],
-        'author'            => [['author-{{slug}}', 'author-{{id}}', 'author'], true],
-        'date'              => [['date'], false],
-        'archive'           => [['archive'], false],
-        'comments_popup'    => [['comments-popup.php'], false],
-        'paged'             => [['paged'], false],
+        '404'               => [
+            ['404'],
+            false,
+        ],
+        'search'            => [
+            ['search'],
+            false,
+        ],
+        'front_page'        => [
+            ['front-page'],
+            false,
+        ],
+        'home'              => [
+            ['home'],
+            false,
+        ],
+        'post_type_archive' => [
+            ['archive'],
+            false,
+        ],
+        'tax'               => [
+            ['taxonomy-{{tax}}-{{slug}}', 'taxonomy-{{tax}}', 'taxonomy'],
+            true,
+        ],
+        'attachment'        => [
+            ['attachment'],
+            false,
+        ],
+        'single'            => [
+            ['single-{{slug}}', 'single', 'singular'],
+            true,
+        ],
+        'page'              => [
+            ['page-{{slug}}', 'page-{{id}}', 'page', 'singular'],
+            true,
+        ],
+        'category'          => [
+            ['category-{{slug}}', 'category-{{id}}', 'category'],
+            true,
+        ],
+        'tag'               => [
+            ['tag-{{slug}}', 'tag-{{id}}', 'tag'],
+            true,
+        ],
+        'author'            => [
+            ['author-{{slug}}', 'author-{{id}}', 'author'],
+            true,
+        ],
+        'date'              => [
+            ['date'],
+            false,
+        ],
+        'archive'           => [
+            ['archive'],
+            false,
+        ],
+        'comments_popup'    => [
+            ['comments-popup.php'],
+            false,
+        ],
+        'paged'             => [
+            ['paged'],
+            false,
+        ],
     ];
 
     /**
@@ -48,6 +104,7 @@ class Hierarchy
     }
 
     /**
+     * @param  bool  $merged
      * @return array
      */
     public function get($merged = false)
@@ -72,7 +129,7 @@ class Hierarchy
      * query type.
      * Callback have to return a non-falsey value to stop the looping through hierarchy array.
      * If template is not found method return FALSE.
-     * If template is found (callback return something not empty) methodd return whatever is
+     * If template is found (callback return something not empty) method return whatever is
      * returned by callback.
      *
      * @param  callable $callable
@@ -129,15 +186,28 @@ class Hierarchy
         return $merged;
     }
 
+    /**
+     * @param  string $branch
+     * @return array
+     * @see getPostTypeArchiveBranches()
+     * @see getArchiveBranches()
+     * @see getPageBranches()
+     * @see getAttachmentBranches()
+     */
     private function typeBranches($branch)
     {
         $branches = $this->getBranches();
-        $tmpls = $branches[$branch][0];
+        $templates = $branches[$branch][0];
         $cb = 'get'.str_replace(' ', '', ucwords(str_replace('_', ' ', $branch))).'Branches';
 
-        return method_exists($this, $cb) ? $this->$cb($tmpls) : $tmpls;
+        return method_exists($this, $cb) ? $this->$cb($templates) : $templates;
     }
 
+    /**
+     * @param  string $branch
+     * @param  array  $hierarchy
+     * @return array
+     */
     private function branchVariables($branch, array $hierarchy)
     {
         $branches = $this->getBranches();
@@ -162,7 +232,11 @@ class Hierarchy
         return ! empty($vars) ? $this->replaceBranchVars($hierarchy, $vars) : $hierarchy;
     }
 
-    private function getPostTypeArchiveBranches(array $tmpls)
+    /**
+     * @param  array $templates
+     * @return array
+     */
+    private function getPostTypeArchiveBranches(array $templates)
     {
         $post_type = get_query_var('post_type');
         if (is_array($post_type)) {
@@ -172,35 +246,46 @@ class Hierarchy
         if (! $obj->has_archive) {
             return [];
         }
-        array_unshift($tmpls, "archive-{$post_type}");
+        array_unshift($templates, "archive-{$post_type}");
 
-        return $tmpls;
+        return $templates;
     }
 
-    private function getArchiveBranches(array $tmpls)
+    /**
+     * @param  array $templates
+     * @return array
+     */
+    private function getArchiveBranches(array $templates)
     {
         $post_types = array_filter((array) get_query_var('post_type'));
         if (count($post_types) == 1) {
             $post_type = array_shift($post_types);
             if (post_type_exists($post_type)) {
-                array_unshift($tmpls, "archive-{$post_type}");
+                array_unshift($templates, "archive-{$post_type}");
             }
         }
 
-        return $tmpls;
+        return $templates;
     }
 
-    private function getPageBranches(array $tmpls)
+    /**
+     * @param  array $templates
+     * @return array
+     */
+    private function getPageBranches(array $templates)
     {
         $file = filter_var(get_page_template_slug(), FILTER_SANITIZE_URL);
         if (! empty($file) && validate_file($file) === 0) {
             $cut = strlen(pathinfo($file, PATHINFO_EXTENSION)) * -1;
-            array_unshift($tmpls, substr($file, 0, $cut < 0 ? $cut - 1 : strlen($file)));
+            array_unshift($templates, substr($file, 0, $cut < 0 ? $cut - 1 : strlen($file)));
         }
 
-        return $tmpls;
+        return $templates;
     }
 
+    /**
+     * @return array
+     */
     private function getAttachmentBranches()
     {
         global $posts;
@@ -211,15 +296,20 @@ class Hierarchy
         if (empty($type)) {
             return ['attachment'];
         }
-        $tmpls = [$type[0]];
+        $templates = [$type[0]];
         if (! empty($type[1])) {
-            $tmpls = array_merge($tmpls, [$type[1], "{$type[0]}_{$type[1]}"]);
+            $templates = array_merge($templates, [$type[1], "{$type[0]}_{$type[1]}"]);
         }
-        $tmpls[] = 'attachment';
+        $templates[] = 'attachment';
 
-        return $tmpls;
+        return $templates;
     }
 
+    /**
+     * @param  array $types
+     * @param  array $vars
+     * @return array
+     */
     private function replaceBranchVars(array $types, array $vars)
     {
         array_walk($types, function (&$branch, $i, $vars) {
